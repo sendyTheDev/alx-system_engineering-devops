56c19a03556b2aebc4b8cd8e9b75a8a0a1743dc8
sendyTheDev <phathisanisendy@gmail.com>
2023-11-14 04:26:52 +0000

    debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#fix our stack so that we get to 0 failed requests
+exec {'sets file limite for nginx':
+  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /etc/default/nginx'
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+exec {'holberton hard':
+path     => ['/usr/bin/'],
+command  => "sudo sed -i 's/^holberton hard /holberton hard nofile 2000/g' /etc/security/limits.conf",
+provider => 'shell',
+}
+
+exec {'holberton soft':
+path     => ['/usr/bin/'],
+command  => "sudo sed -i 's/^holberton soft /holberton soft nofile 2000/g' /etc/security/limits.conf",
+provider => 'shell',
+}
